e970e1eb6fbac144be1bca4bec101827b5506e7a
Neil Herriford <neil.herriford@gmail.com>
2019-12-08 00:25:53 -0500

    Add destructuring / create polyline to factory

--- app/javascript/packs/geometry/LineFactory.js
+++ app/javascript/packs/geometry/LineFactory.js
@@ -1,3 +1,4 @@
+import {equal as pointsEqual} from './Point';
 let nextLineId = -1;
 const LINES = {};
 
@@ -7,6 +8,40 @@ class LineFactory {
     const line =  {id: id, head: [...head], tail: [...tail]};
     LINES[id] = line;
     return line;
+  }
+
+  static destructurePolyline(polyline) {
+    if(polyline === null) return [];
+    if(polyline.length === 0) return [];
+
+    const result = [
+      polyline[0].head,
+      polyline[0].tail
+    ];
+
+    for(let i = 1; i < polyline.length; ++i) {
+      let current = polyline[i];
+      let previous = result[result.length - 1];
+
+      if(!pointsEqual(previous, current.head)) result.push(current.head);
+      if(!pointsEqual(previous, current.tail)) result.push(current.tail);
+    }
+
+    return result;
+  }
+
+  static createPolyline(points) {
+    if(points === null) return null;
+    if(points.length === 1) return [];
+    let result = [];
+
+    for(let i = 1; i < points.length; ++i) {
+      let current = points[i];
+      let previous = points[i - 1];
+      result.push(LineFactory.createLine(previous, current));
+    }
+
+    return result;
   }
 
   static getLineById(id) {
