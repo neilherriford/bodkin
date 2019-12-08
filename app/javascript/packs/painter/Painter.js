de1c4bde2e94267e26aa1510dc2a312ed5f081a0
Neil Herriford <neil.herriford@gmail.com>
2019-12-07 22:52:15 -0500

    Clean up painter Take in styles with supplied defaults and add filling polygons (bounding boxes being a special case) and labeling

--- app/javascript/packs/painter/Painter.js
+++ app/javascript/packs/painter/Painter.js
@@ -1,5 +1,5 @@
 import {QUADRANT_TYPE} from '../pmtree/Quadrant';
-import Geometry from '../geometry/Geometry';
+import {offset as offsetPoint} from '../geometry/Point';
 
 class Painter {
   constructor(ctx) {
@@ -12,32 +12,24 @@ class Painter {
     this.ctx.clearRect(0, 0, width, height);
   }
 
-  drawPolyline(polyline) {
-    for(let line of polyline) this.drawLine(line);
+  drawPolyline(polyline, lineStyle='DarkGreen', fillStyle='LightCoral') {
+    for(let line of polyline) this.drawLine(line, lineStyle, fillStyle);
   }
 
-  drawLine(line, color='SaddleBrown') {
+  drawLine(line, lineStyle='DarkGreen', fillStyle='LightCoral') {
     if(line === null) return;
-    this.ctx.strokeStyle = 'SaddleBrown';
-    this.ctx.fillStyle = 'SaddleBrown';
 
+    this.ctx.lineStyle = lineStyle;
+    this.ctx.fillStyle = fillStyle;
     this.ctx.beginPath();
     this.ctx.moveTo(...line.head);
     this.ctx.lineTo(...line.tail);
     this.ctx.stroke();
-    this.fillCircle(line.head, 1);
-    this.fillCircle(line.tail, 1);
-
-    const midpoint = [
-      (line.head[0] + line.tail[0]) / 2,
-      (line.head[1] + line.tail[1]) / 2,
-    ];
-
-    this.ctx.fillStyle = 'orange';
-    ctx.fillText(line.id, ...midpoint);
+    this.fillCircle(line.head, 0.25);
+    this.fillCircle(line.tail, 0.25);
   }
 
-  fillCircle(center, radius) {
+  fillCircle(center, radius, lineStyle='DarkGreen', fillStyle='LightCoral') {
     this.ctx.beginPath();
     this.ctx.arc(...center, radius, 0, 2 * Math.PI);
     this.ctx.closePath();
@@ -88,6 +80,33 @@ class Painter {
     const width = highlight.bounds.halfWidth * 2;
     this.ctx.strokeStyle = 'Fuchsia';
     this.ctx.strokeRect(...highlight.bounds.nw, width, width);
+  }
+
+  fillPolygon(points, lineStyle='DarkGreen', fillStyle='LightCoral') {
+    this.ctx.lineStyle = lineStyle;
+    this.ctx.fillStyle = fillStyle;
+    this.ctx.beginPath();
+    this.ctx.moveTo(...points[0]);
+    for(let index = 1; index <= points.length; ++index) {
+      this.ctx.lineTo(...points[index % points.length]);
+    }
+    this.ctx.closePath();
+    this.ctx.fill();
+    this.ctx.stroke();
+  }
+
+  label(location, text, tyle='violet') {
+    this.ctx.fillStyle = style;
+    this.ctx.fillText(text, ...location);
+  }
+
+  fillBounds(bounds, lineStyle='DarkGreen', fillStyle='LightCoral') {
+    this.fillPolygon([
+      bounds.nw,
+      bounds.ne,
+      bounds.se,
+      bounds.sw
+    ]);
   }
 }
 
